dabb36636f0113b92e2f9b4196ccfa026ca2f2e5
Th3VaultDweller <aztec242@yandex.ru>
2023-12-07 22:17:09 +0300

    Added headers and html code

--- vkusvill_parser.py
+++ vkusvill_parser.py
@@ -1,3 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
+headers = {
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+}
+# получаем код страницы товаров
+html = requests.get("https://vkusvill.ru/goods/").text
+print(html)
+
+soup = BeautifulSoup(html, "lxml")  # lxml - самый быстрый парсер
