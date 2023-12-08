9d6af07e667a7d47e955140fa591ae8c3701e3a4
Th3VaultDweller <aztec242@yandex.ru>
2023-12-08 21:01:04 +0300

    Proxy test

--- vkusvill_parser.py
+++ vkusvill_parser.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-}
-# получаем код страницы товаров
-html = requests.get("https://vkusvill.ru/goods/").text
-print(html)
+# headers = {
+#     "Accept": "application/json",
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+# }
 
-soup = BeautifulSoup(html, "lxml")  # lxml - самый быстрый парсер
+proxy = {"https": "https://91.25.93.174:3128"}
+
+# получаем код страницы товаров
+url = "http://ident.me/"
+response = requests.get(url, proxies=proxy)
+ip_adress = response.text
+print("Your IP is:", ip_adress)
+
+
+# soup = BeautifulSoup(html, "lxml")  # lxml - самый быстрый парсер
