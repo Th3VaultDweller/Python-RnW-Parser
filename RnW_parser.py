9eefe9cf2e2d10483af1140e2be07b49febec924
Th3VaultDweller <aztec242@yandex.ru>
2023-12-20 20:29:38 +0300

    New import

--- RnW_parser.py
+++ RnW_parser.py
@@ -1,18 +1,9 @@
+from time import sleep
+
 import requests
 from bs4 import BeautifulSoup
-
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-}
-
-
-# получаем код страницы товаров
-html = requests.get("https://krasnoeibeloe.ru/catalog/").text
-
-with open("data/catalog.html", "w") as file:
-    file.write(html)  # запишем код страницы в отдельный файл на всякий случай
-
-soup = BeautifulSoup(html, "lxml")  # lxml - самый быстрый парсер
-
-# получаем все ссылки со страницы товаров
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
