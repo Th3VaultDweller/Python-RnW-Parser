900842a6dbac6f6fa6468e36d45bc9d130d34373
Th3VaultDweller <aztec242@yandex.ru>
2023-12-20 21:13:40 +0300

    Testing webdriver

--- RnW_parser.py
+++ RnW_parser.py
@@ -1,9 +1,15 @@
-from time import sleep
+import time
 
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as wait
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.com/")
+time.sleep(5)
+driver.quit()
