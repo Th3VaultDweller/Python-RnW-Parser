e1c6ee90cc39277b0f77e2ea62b8119d91c4a1b6
Th3VaultDweller <aztec242@yandex.ru>
2024-01-29 23:31:20 +0300

    Added options and age verification pass

--- RnW_parser.py
+++ RnW_parser.py
@@ -5,13 +5,24 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as wait
+from webdriver_manager.chrome import ChromeDriverManager
 
-# берём драйвер для работы Selenium и запускаем Chrome на странице поиска
-browser = webdriver.Chrome()
+# делаем опцию для запуска браузера в режиме инкогнито
+option = Options()
+option = webdriver.ChromeOptions()
+option.add_argument("--incognito")
+option.add_argument("--disable-infobars")
+option.add_argument("--start-maximized")
+
+# берём драйвер для работы Selenium и запускаем Chrome на странице поиска в режиме инкогнито
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+browser = webdriver.Chrome(options=option)
 browser.get("https://www.google.com/")
 
 # определяем URL сайта
@@ -20,12 +31,22 @@ browser.get(url)
 time.sleep(15)
 
 # ждём появления кнопки и проходим проверку на возраст
-element = wait(browser, 15).until(
-    EC.presence_of_all_elements_located(
-        (By.XPATH, "/html/body/div[5]/div/div/div/div/div/div[2]/div[2]/a[1]")
+age_popup = (
+    wait(browser, 30)
+    .until(
+        EC.visibility_of_element_located(
+            (By.XPATH, "/html/body/div[7]/div/div/div/div/div/div[2]/div[2]/a[1]")
+        )
     )
+    .click()
 )
-element.click()
+
+time.sleep(15)
+
+# переходим в каталог
+catalogue = browser.find_element(
+    By.XPATH, "/html/body/div[1]/div/header/div/div[2]/div[3]/nav/ul/li[1]/a"
+).click()
 
 time.sleep(15)
 
