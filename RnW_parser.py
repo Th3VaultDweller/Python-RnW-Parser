ab9c3e2818d1bbed52574862d9a7063bb856efd7
Th3VaultDweller <aztec242@yandex.ru>
2024-01-29 23:37:49 +0300

    Updated time.sleep

--- RnW_parser.py
+++ RnW_parser.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 import requests
@@ -28,11 +29,11 @@ browser.get("https://www.google.com/")
 # определяем URL сайта
 url = "https://krasnoeibeloe.ru/"
 browser.get(url)
-time.sleep(15)
+time.sleep(random.randrange(5, 15))
 
 # ждём появления кнопки и проходим проверку на возраст
 age_popup = (
-    wait(browser, 30)
+    wait(browser, 60)
     .until(
         EC.visibility_of_element_located(
             (By.XPATH, "/html/body/div[7]/div/div/div/div/div/div[2]/div[2]/a[1]")
@@ -41,13 +42,16 @@ age_popup = (
     .click()
 )
 
-time.sleep(15)
+time.sleep(random.randrange(5, 15))
 
 # переходим в каталог
 catalogue = browser.find_element(
     By.XPATH, "/html/body/div[1]/div/header/div/div[2]/div[3]/nav/ul/li[1]/a"
 ).click()
 
-time.sleep(15)
+time.sleep(random.randrange(5, 15))
+
+# и пробуем собрать все товары из одной категории
+
 
 browser.quit()
