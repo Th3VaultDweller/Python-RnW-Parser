7846081db66c3ea377c7eea6d3a09491d104a145
Th3VaultDweller <aztec242@yandex.ru>
2024-01-29 19:03:17 +0300

    Trying out new methods of clicking buttons

--- RnW_parser.py
+++ RnW_parser.py
@@ -5,17 +5,28 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as wait
 
-# берём драйвер для работы Selenium
+# берём драйвер для работы Selenium и запускаем Chrome на странице поиска
 browser = webdriver.Chrome()
 browser.get("https://www.google.com/")
 
 # определяем URL сайта
 url = "https://krasnoeibeloe.ru/"
 browser.get(url)
+time.sleep(15)
 
-time.sleep(5)
+# ждём появления кнопки и проходим проверку на возраст
+element = wait(browser, 15).until(
+    EC.presence_of_all_elements_located(
+        (By.XPATH, "/html/body/div[5]/div/div/div/div/div/div[2]/div[2]/a[1]")
+    )
+)
+element.click()
+
+time.sleep(15)
+
 browser.quit()
