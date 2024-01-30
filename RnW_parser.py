c9a96b41ea84af8f72cd796f265c218fdf3efa6c
Th3VaultDweller <aztec242@yandex.ru>
2024-01-30 21:03:01 +0300

    Modified the code and added for loop

--- RnW_parser.py
+++ RnW_parser.py
@@ -31,30 +31,42 @@ url = "https://krasnoeibeloe.ru/"
 browser.get(url)
 
 # ждём появления кнопки и проходим проверку на возраст
-age_popup = (
-    wait(browser, 60)
-    .until(
-        EC.visibility_of_element_located(
-            (By.XPATH, "/html/body/div[7]/div/div/div/div/div/div[2]/div[2]/a[1]")
-        )
-    )
-    .click()
-)
+element_to_be_clicked = "/html/body/div[7]/div/div/div/div/div/div[2]/div[2]/a[1]"
 
-time.sleep(random.randrange(5, 15))
+if element_to_be_clicked:
+    age_popup = (
+        wait(browser, 60)
+        .until(EC.visibility_of_element_located((By.XPATH, element_to_be_clicked)))
+        .click()
+    )
+else:
+    pass
+    time.sleep(random.randrange(2, 5))
+
 
 # переходим в каталог
 catalogue = browser.find_element(
     By.XPATH, "/html/body/div[1]/div/header/div/div[2]/div[3]/nav/ul/li[1]/a"
 ).click()
 
-time.sleep(random.randrange(5, 15))
+time.sleep(random.randrange(2, 5))
 
 # и пробуем собрать все товары из одной категории
 category = browser.find_element(
     By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div[2]/div[1]/div[2]/a"
-).click()
+)
+category.click()
 
 time.sleep(random.randrange(5, 15))
+
+all_product_links = browser.find_element(
+    By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div[6]"
+).find_elements("catalog_product_item").text()
+for link in all_product_links:
+    link_text = all_product_links.get_attribute("text")  # название товара
+    link_href = all_product_links.get_attribute("href")  # ccылка на товар
+    # print(i)  # нумерация товаров начинается с нуля
+    print(f"{link_text.strip()}: {link_href}")
+
 
 browser.quit()
