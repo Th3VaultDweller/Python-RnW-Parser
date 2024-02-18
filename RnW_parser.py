a73ddc98c7995411a8ee117528bd5da13d9f773d
Th3VaultDweller <aztec242@yandex.ru>
2024-02-19 01:13:52 +0300

    Added if statement for yet-to-be-done category of goods

--- RnW_parser.py
+++ RnW_parser.py
@@ -51,25 +51,25 @@ else:
 
 print("Перехожу в каталог товаров...\n")
 
-# # находим каталог товаров на странице
-# all_categories = browser.find_element(By.CLASS_NAME, "left_catalog_c").find_elements(
-#     By.TAG_NAME, "a"
-# )  # все категории товаров на сайте
+# находим каталог товаров на странице
+all_categories = browser.find_element(By.CLASS_NAME, "left_catalog_c").find_elements(
+    By.TAG_NAME, "a"
+)  # все категории товаров на сайте
 
-# # берём каждую ссылку и каталога товаров
-# print(f"Вывожу на экран все категории товаров и сохраняю их в файл...\n")
-# all_categories_links_dict = {}
-# for link in all_categories:
-#     link_text = link.text
-#     link_href = link.get_attribute("href")
-#     print(f"{link_text}: {link_href}")
-#     time.sleep(random.randrange(2, 5))
+# берём каждую ссылку и каталога товаров
+print(f"Вывожу на экран все категории товаров и сохраняю их в файл...\n")
+all_categories_links_dict = {}
+for link in all_categories:
+    link_text = link.text
+    link_href = link.get_attribute("href")
+    print(f"{link_text}: {link_href}")
+    time.sleep(random.randrange(2, 5))
 
-#     all_categories_links_dict[link_text] = link_href
+    all_categories_links_dict[link_text] = link_href
 
-#     # сохраняем каждую ссылку из каталога товаров в отдельный json-файл
-#     with open("all_categories_links.json", "w") as file:
-#         json.dump(all_categories_links_dict, file, indent=4, ensure_ascii=False)
+    # сохраняем каждую ссылку из каталога товаров в отдельный json-файл
+    with open("all_categories_links.json", "w") as file:
+        json.dump(all_categories_links_dict, file, indent=4, ensure_ascii=False)
 
 # открываем сохранённый файл
 with open("all_categories_links.json") as file:
@@ -83,65 +83,71 @@ for category in list(all_links.values()):
     category_inner_name = browser.find_element(By.TAG_NAME, "h1").text
     time.sleep(random.randrange(2, 5))
     print(f"Выбираю категорию товара <<{category_inner_name}>>\n")
-
-    # и пробуем собрать все товары из одной категории
-    all_product_links = browser.find_elements(
-        By.CLASS_NAME, "catalog_product_item_cont"
-    )
-    if all_product_links:
-        time.sleep(random.randrange(5, 10))
-        print("Начинаю парсинг информации...\n")
-
-        for i, link in enumerate(all_product_links):
-            link_text = (
-                link.find_element(By.CLASS_NAME, "product_item_name")
-                .find_element(By.TAG_NAME, "a")
-                .text
-            )  # название товара
-
-            link_href = (
-                link.find_element(By.CLASS_NAME, "product_item_name")
-                .find_element(By.TAG_NAME, "a")
-                .get_property("href")
-            )  # ccылка на товар
-
-            link_subtitle = (
-                link.find_element(By.CLASS_NAME, "product_item_name")
-                .find_element(By.CLASS_NAME, "product-subtitle")
-                .text
-            )  # сопутствующая информация о товаре
-
-            link_rating = (
-                link.find_element(By.CLASS_NAME, "product_item_bottom")
-                .find_element(By.CLASS_NAME, "rate_votes")
-                .text
-            )  # количество оценок товара
-
-            print(i)  # нумерация товаров начинается с нуля
-
-            link_price = link.find_element(
-                By.CLASS_NAME, "product_item_bottom-btns"
-            )  # цена на товар
-
-            if link_price.find_element(By.TAG_NAME, "a").text:
-                print(f"Цену товара можно уточнить в магазине.")
-            else:
-                product_price = link_price.find_elements(
-                    By.CLASS_NAME, "i_price"
-                )  # цена товара
-                for price in product_price:
-                    if product_price:
-                        link_price = price.find_element(By.TAG_NAME, "div").text
-
-            print(
-                f"Название: {link_text}\nСтрана, объём и процент алкоголя: {link_subtitle}\nКоличество оценок: {link_rating}\nЦена: {link_price}\nСсылка: {link_href}\n"
-            )
-
-            time.sleep(10)
-
-    else:
-        print(f"В категории <<{category_inner_name}>> нет информации для парсинга :(\n")
+    
+    if category_inner_name == "Идеи для подарков":
+        print(f"Скрипт для <<{category_inner_name}>> в процессе написания!\n")
         pass
+    else:
+        # пробуем собрать все товары из одной категории
+        all_product_links = browser.find_elements(
+            By.CLASS_NAME, "catalog_product_item_cont"
+        )
+        if all_product_links:
+            time.sleep(random.randrange(5, 10))
+            print("Начинаю парсинг информации...\n")
+
+            for i, link in enumerate(all_product_links):
+                link_text = (
+                    link.find_element(By.CLASS_NAME, "product_item_name")
+                    .find_element(By.TAG_NAME, "a")
+                    .text
+                )  # название товара
+
+                link_href = (
+                    link.find_element(By.CLASS_NAME, "product_item_name")
+                    .find_element(By.TAG_NAME, "a")
+                    .get_property("href")
+                )  # ccылка на товар
+
+                link_subtitle = (
+                    link.find_element(By.CLASS_NAME, "product_item_name")
+                    .find_element(By.CLASS_NAME, "product-subtitle")
+                    .text
+                )  # сопутствующая информация о товаре
+
+                link_rating = (
+                    link.find_element(By.CLASS_NAME, "product_item_bottom")
+                    .find_element(By.CLASS_NAME, "rate_votes")
+                    .text
+                )  # количество оценок товара
+
+                print(i)  # нумерация товаров начинается с нуля
+
+                link_price = link.find_element(
+                    By.CLASS_NAME, "product_item_bottom-btns"
+                )  # цена на товар
+
+                if link_price.find_element(By.TAG_NAME, "a").text:
+                    print(f"Цену товара можно уточнить в магазине.")
+                else:
+                    product_price = link_price.find_elements(
+                        By.CLASS_NAME, "i_price"
+                    )  # цена товара
+                    for price in product_price:
+                        if product_price:
+                            link_price = price.find_element(By.TAG_NAME, "div").text
+
+                print(
+                    f"Название: {link_text}\nСтрана, объём и процент алкоголя: {link_subtitle}\nКоличество оценок: {link_rating}\nЦена: {link_price}\nСсылка: {link_href}\n"
+                )
+
+                time.sleep(10)
+
+        else:
+            print(
+                f"В категории <<{category_inner_name}>> нет информации для парсинга :(\n"
+            )
+            pass
 
 finish_app_time = timer()
 
